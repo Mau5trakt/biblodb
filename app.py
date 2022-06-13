880e374912c74c4ecd91029283c339b9805a1ba1
Mau5trakt <kubzalemany@hotmail.com>
2022-06-12 21:06:44 -0600

    branch master

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return '<h1>Solo accesible desde develop</h1>'
+    return '<h1>Solo accesible desde main</h1>'
 
 
 if __name__ == '__main__':
