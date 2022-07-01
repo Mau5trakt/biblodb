2fb0257eef7556f80b5f464742455575d8b83027
Mau5trakt <80871251+Mau5trakt@users.noreply.github.com>
2022-07-01 20:06:09 +0000

    Testing some things

--- consultitas.py
+++ consultitas.py
@@ -1,51 +1,7 @@
-#from objs import ver_press, libros_disponibles
-#from functions import ver_press, libros_disponibles, vencido
-from datetime import timedelta, datetime, date
-from functions import verificar_vencido, verificar_disponibles, verificar_enprestamo, verificar_nolotenga, fecha_prestamo
+from functions import *
 
-'''
-verificadores = []
-#Ver si tiene prestamos vencidos
-ver1 = verificar_vencido("20190881")
-verificadores.append(ver1)
-#Ver si hay mas de 3 unidades de este libro
-ver2 = verificar_disponibles(9789681863654)
-verificadores.append(ver2)
-#Ver si el usuario tiene mas de 3 prestamos activos
-ver3 = verificar_enprestamo("20190881")
-verificadores.append(ver3)
+isbn = 9780131146808
+carnet = 20200880
 
-#ver que no sea el mismo libro
-ver4 = verificar_nolotenga(20190881, 9789681863654)
-verificadores.append(ver4)
-
-print(verificadores)
-if False in verificadores:
-    print("No se puede realizar el prestamo")
-#lista = [False,True, True]
-'''
-#print(str(date.today()))
-'''
-hoy = date.today()
-agg = timedelta(days=7)
-print(hoy + agg)
-'''
-
-print(type(fecha_prestamo()))
-
-#semana = hoy + datetime.timedelta(days=7)
-
-
-#hoy = datetime.strptime(str(date.today()), "%Y-%m-%d")
-#semana = hoy + 7
-#print(semana)
-
-
-#if True in lista:
-#    print("Falso")
-
-
-
-
-
-
+aver = tramites()
+print(aver)
