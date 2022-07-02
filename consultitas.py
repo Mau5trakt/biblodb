f7ae626dc3f10ef4b271753b7e97acfb0222152a
Mau5trakt <kubzalemany@hotmail.com>
2022-07-02 00:19:09 -0600

    update!: another test file

--- consultitas.py
+++ consultitas.py
@@ -1,7 +1,6 @@
-from functions import *
+from cs50 import SQL
 
-isbn = 9780131146808
-carnet = 20200880
+db = SQL("sqlite:///database/respaldo.db")
 
-aver = tramites()
-print(aver)
+sacar_id = db.execute("SELECT libro_id from prestamo where id_prestamo = 17")[0]["libro_id"]
+print(sacar_id)
